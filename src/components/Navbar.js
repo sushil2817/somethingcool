2c89e6863b2f8afdf460e609dcb0193c66fcf13a
sushil2817 <tony281723@gmail.com>
2024-05-05 23:30:37 +0530

    Creating something cool designs and tamplates

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+             <div>
+                 <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Support</li>
+                 </ul>
+             </div>
+    </div>
+  )
+}
+
+export default Navbar
